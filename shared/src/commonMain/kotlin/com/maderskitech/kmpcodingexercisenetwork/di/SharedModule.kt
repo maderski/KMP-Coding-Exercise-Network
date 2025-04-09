d46b4abaa799785ae952928210893bb09fd2b411
Jason Maderski <maderski@gmail.com>
2025-04-09 16:35:20 -0400

    remove default unit tests

--- shared/src/commonMain/kotlin/com/maderskitech/kmpcodingexercisenetwork/di/SharedModule.kt
+++ shared/src/commonMain/kotlin/com/maderskitech/kmpcodingexercisenetwork/di/SharedModule.kt
@@ -1,7 +1,12 @@
 package com.maderskitech.kmpcodingexercisenetwork.di
 
+import com.maderskitech.kmpcodingexercisenetwork.data.remote.api.ItemsApi
+import com.maderskitech.kmpcodingexercisenetwork.data.remote.api.network.HttpClientFactory
+import io.ktor.client.HttpClient
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val sharedModule = module {
-
+    single<HttpClient> { HttpClientFactory.create(get()) }
+    singleOf(::ItemsApi)
 }
