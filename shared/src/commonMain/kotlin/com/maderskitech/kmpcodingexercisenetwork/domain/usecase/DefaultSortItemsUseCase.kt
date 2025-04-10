7420f8848389fd3fd0cc87db78f674da4f7c0f3f
Jason Maderski <maderski@gmail.com>
2025-04-09 22:36:56 -0400

    add comments to explain logic in the DefaultSortItemsUseCase

--- shared/src/commonMain/kotlin/com/maderskitech/kmpcodingexercisenetwork/domain/usecase/DefaultSortItemsUseCase.kt
+++ shared/src/commonMain/kotlin/com/maderskitech/kmpcodingexercisenetwork/domain/usecase/DefaultSortItemsUseCase.kt
@@ -9,9 +9,16 @@ class DefaultSortItemsUseCase(private val repository: ItemRepository) : SortedIt
     override fun getSortedItemsFlow(): Flow<Result<Map<Int, List<Item>>>> = flow {
         repository.getAllItems()
             .onSuccess { items ->
+                /*
+                I suspected the Kotlin StdLib had to have the functionality to sort the results the
+                way I wanted.  Since I am not supposed to use AI, I searched stack
+                overflow and found the answer.
+
+                link: https://stackoverflow.com/questions/61021088/kotlin-sort-list-of-objects-by-their-id-and-parentid
+                 */
                 val sortedItemsMap = items
-                    .sortedWith(compareBy({ it.listId }, { it.name }))
-                    .groupBy { it.listId }
+                    .sortedWith(compareBy({ it.listId }, { it.name })) // Sorts the list of items by ListId followed by the name.
+                    .groupBy { it.listId } // Groups the items by listId, thus creating a map with the listId as the key and a list of items with the same listId as the value.
                 emit(Result.success(sortedItemsMap))
             }.onFailure { throwable ->
                 emit(Result.failure(throwable))
