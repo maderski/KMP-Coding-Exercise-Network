60c8a45a20bdf7c06e2cdccb608deb3b894eff66
Jason Maderski <maderski@gmail.com>
2025-04-16 15:57:29 -0400

    move initKoin into a companion object inside of KoinHelper

--- shared/src/commonMain/kotlin/com/maderskitech/kmpcodingexercisenetwork/di/KoinHelper.kt
+++ shared/src/commonMain/kotlin/com/maderskitech/kmpcodingexercisenetwork/di/KoinHelper.kt
@@ -1,21 +1,34 @@
 package com.maderskitech.kmpcodingexercisenetwork.di
 
+import com.maderskitech.kmpcodingexercisenetwork.domain.respository.ItemRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
+import kotlin.jvm.JvmStatic
 
-fun initKoin(
-    appModules: List<Module> = emptyList(),
-    config: KoinAppDeclaration? = null
-) {
-    startKoin {
-        printLogger()
-        config?.invoke(this) // optional custom setup
-        modules(
-            buildList {
-                add(commonModule)
-                addAll(appModules)
+class KoinHelper : KoinComponent {
+    private val itemRepository: ItemRepository  by inject()
+
+    fun getItemRepository(): ItemRepository = itemRepository
+
+    companion object {
+        @JvmStatic
+        fun initKoin(
+            appModules: List<Module> = emptyList(),
+            config: KoinAppDeclaration? = null
+        ) {
+            startKoin {
+                printLogger()
+                config?.invoke(this) // optional custom setup
+                modules(
+                    buildList {
+                        add(commonModule)
+                        addAll(appModules)
+                    }
+                )
             }
-        )
+        }
     }
 }
